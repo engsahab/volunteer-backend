951846fec0d87a0dbcb1c1ac111c21bca41e5a79
engsahab <sahabalfahad6@gmail.com>
2025-10-27 15:25:21 +0300

    Register Application model in admin

--- volunteer_app/admin.py
+++ volunteer_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Opportunity
+from .models import Opportunity, Application
 # Register your models here.
 
 admin.site.register(Opportunity)
+admin.site.register(Application)
