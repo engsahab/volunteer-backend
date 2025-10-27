95b1e0a3615cd160cb693bff4e88a68407fdce8a
engsahab <sahabalfahad6@gmail.com>
2025-10-27 13:12:24 +0300

    feat: Add OpportunityList and OpportunityDetail views

--- volunteer_app/views.py
+++ volunteer_app/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
+from .models import Opportunity
+from .serializers import OpportunitySerializer
 # Create your views here.
 
 class Home(APIView):
-    
     def get(self, request):
         content = {'message': 'Welcome to the Volunteer Management API!'}
         return Response(content)
@@ -17,3 +20,63 @@ class Home(APIView):
             'data_received': request.data 
         }
         return Response(content)
+    
+
+
+class OpportunityList(APIView):
+    def get(self, request):
+        queryset = Opportunity.objects.all()
+        serializer = OpportunitySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        try:
+            serializer = OpportunitySerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        
+
+
+class OpportunityDetail(APIView):
+    def get(self, request, opportunity_id):
+        try:
+            queryset = get_object_or_404(Opportunity, id=opportunity_id)
+            serializer = OpportunitySerializer(queryset)
+            return Response(serializer.data)
+
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def put(self, request, opportunity_id):
+        try:
+            queryset = get_object_or_404(Opportunity, id=opportunity_id)
+            serializer = OpportunitySerializer(queryset, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def delete(self, request, opportunity_id):
+        try:
+            queryset = get_object_or_404(Opportunity, id=opportunity_id)
+            queryset.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        except Exception as error:
+            return Response(
+                {"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
