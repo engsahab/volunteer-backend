062094a1da00e456cd5c1fa440c54a481faab325
engsahab <sahabalfahad6@gmail.com>
2025-10-27 15:26:38 +0300

    Add ApplicationSerializer

--- volunteer_app/serializers.py
+++ volunteer_app/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Opportunity
+from .models import Opportunity,Application
 
 
 class OpportunitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Opportunity
+        fields = '__all__'
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
         fields = '__all__'
