f0b5dca7e447d183e9f76590a99db16fee5def2a
engsahab <sahabalfahad6@gmail.com>
2025-10-27 13:14:04 +0300

    Add URL routes for Opportunity CRUD

--- volunteer_app/urls.py
+++ volunteer_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    
+   
     path('home/', views.Home.as_view(), name='home'),
+
+    path('opportunities/', views.OpportunityList.as_view(), name='opportunity_list'),
+    path('opportunities/<int:opportunity_id>/', views.OpportunityDetail.as_view(), name='opportunity_detail'),
 ]
