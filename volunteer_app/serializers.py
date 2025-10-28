e47fe1d82b87136cb9b3f95694822456c8f86bed
engsahab <sahabalfahad6@gmail.com>
2025-10-28 11:29:21 +0300

    Add SkillSerializer and update OpportunitySerializer

--- volunteer_app/serializers.py
+++ volunteer_app/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-from .models import Opportunity,Application
+from .models import Opportunity,Application,Skill
 
 
-class OpportunitySerializer(serializers.ModelSerializer):
+
+class SkillSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Opportunity
+        model = Skill
         fields = '__all__'
 
+class OpportunitySerializer(serializers.ModelSerializer):
+    skills = SkillSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Opportunity
+        fields = ['id', 'title', 'description', 'date', 'location', 'skills']
+        
 class ApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Application
