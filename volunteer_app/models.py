4d8f989ed0dcc3a23fa01e0c147352115a9b300d
engsahab <sahabalfahad6@gmail.com>
2025-10-28 11:26:51 +0300

    Add Skill model and M2M to Opportunity

--- volunteer_app/models.py
+++ volunteer_app/models.py
@@ -3,11 +3,15 @@ from django.db import models
 
 # Create your models here.
 
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+
 class Opportunity(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField()
     location = models.CharField(max_length=200)
+    skills = models.ManyToManyField(Skill, blank=True)
 
     def __str__(self):
         return self.title
