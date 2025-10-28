1077daf2a141b585df09bb5cb64f0400ec4a7c23
engsahab <sahabalfahad6@gmail.com>
2025-10-28 11:28:08 +0300

    Register Skill model in admin

--- volunteer_app/admin.py
+++ volunteer_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Opportunity, Application
+from .models import Opportunity, Application,Skill
 # Register your models here.
 
 admin.site.register(Opportunity)
 admin.site.register(Application)
+admin.site.register(Skill)
