2d69a3ed60f2bc2553344a0b8cfd2f05ea61cbd4
engsahab <sahabalfahad6@gmail.com>
2025-11-02 12:59:54 +0300

    update class GetUserProfileView(APIView)

--- volunteer_app/views.py
+++ volunteer_app/views.py
@@ -292,26 +292,22 @@ class GetUserProfileView(APIView):
     permission_classes = [IsAuthenticated] 
 
     def get(self, request):
-        
-       def get(self, request):
-        
+
         user = request.user
         try:
             profile = VolunteerProfile.objects.get(user=user)
         except VolunteerProfile.DoesNotExist:
             profile = VolunteerProfile.objects.create(user=user)
        
-        
         serializer = VolunteerProfileSerializer(profile)
+
         return Response(serializer.data, status=status.HTTP_200_OK)
         
     
     def put(self, request):
-        
+       
         try:
             profile = get_object_or_404(VolunteerProfile, user=request.user)
-
-
             serializer = VolunteerProfileSerializer(profile, data=request.data, partial=True)
 
             if serializer.is_valid():
@@ -321,5 +317,5 @@ class GetUserProfileView(APIView):
 
         except Exception as error:
              return Response({"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-
+    
+    
